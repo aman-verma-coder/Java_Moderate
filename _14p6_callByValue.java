69a757261ab55711257cc2caf6b518691f399f0b
Aman Verma <9330nama@gmail.com>
2024-05-31 15:30:14 +0530

    _14p6_callByValue.java

---
+++ _14p6_callByValue.java
@@ -0,0 +1,16 @@
+public class _14p6_callByValue {
+    public static void swap(int a, int b) {
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        swap(a, b);
+    }
+}
