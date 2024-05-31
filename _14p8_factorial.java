53ba5af312e4c54f3629c65d2ccefeecdb57831b
Aman Verma <9330nama@gmail.com>
2024-05-31 16:00:13 +0530

    _14p8_factorial.java

---
+++ _14p8_factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class _14p8_factorial {
+    public static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        System.out.println("Factorial: " + factorial(n));
+        sc.close();
+    }
+}
