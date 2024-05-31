d77b31a6f75c2c74fe2d25b700a0c741810ed56d
Aman Verma <9330nama@gmail.com>
2024-05-31 13:23:18 +0530

    _14p2_functionSyntax.java

---
+++ _14p2_functionSyntax.java
@@ -0,0 +1,10 @@
+public class _14p2_functionSyntax {
+    public static void prnthlowrld() {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        prnthlowrld();
+    }
+}
