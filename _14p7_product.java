36ebe23a9eb8481cc4f528d48937e3a5305e64e5
Aman Verma <9330nama@gmail.com>
2024-05-31 15:46:13 +0530

    _14p7_product.java

---
+++ _14p7_product.java
@@ -0,0 +1,12 @@
+public class _14p7_product {
+    public static int product(int a, int b) {
+        return (a * b);
+    }
+
+    public static void main(String[] args) {
+        int a, b;
+        a = 5;
+        b = 10;
+        System.out.println("Produt:" + product(a, b));
+    }
+}
