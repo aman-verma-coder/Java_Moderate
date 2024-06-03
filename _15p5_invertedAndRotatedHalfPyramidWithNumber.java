6f30f7191a9fdf70cec9ee020b5f67272489a411
Aman Verma <9330nama@gmail.com>
2024-06-03 22:35:52 +0530

    _15p5_invertedAndRotatedHalfPyramidWithNumber.java

---
+++ _15p5_invertedAndRotatedHalfPyramidWithNumber.java
@@ -0,0 +1,25 @@
+public class _15p5_invertedAndRotatedHalfPyramidWithNumber {
+    public static void numpyramid(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void numpyramid2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        numpyramid(n);
+        numpyramid2(n);
+    }
+}
