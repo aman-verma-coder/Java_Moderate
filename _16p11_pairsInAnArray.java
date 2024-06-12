1005b153d370627a2e3e04645bc85cb1b5b2c1e1
Aman Verma <9330nama@gmail.com>
2024-06-12 16:04:10 +0530

    updating _16p11_pairsInAnArray.java

--- _16p11_pairsInAnArray.java
+++ _16p11_pairsInAnArray.java
@@ -1,11 +1,14 @@
 public class _16p11_pairsInAnArray {
     public static void pair(int num[]) {
+        int tp = 0;
         for (int i = 0; i < num.length; i++) {
             for (int j = i + 1; j < num.length; j++) {
                 System.out.print("(" + num[i] + ", " + num[j] + ")");
+                tp++;
             }
             System.out.println();
         }
+        System.out.println("Total pairs: " + tp);
     }
 
     public static void main(String[] args) {
