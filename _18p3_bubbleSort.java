15f8f715060cfecddb0eb93aa650628828939254
Aman Verma <9330nama@gmail.com>
2024-06-13 18:53:43 +0530

    _18p3_bubbleSort.java

---
+++ _18p3_bubbleSort.java
@@ -0,0 +1,21 @@
+public class _18p3_bubbleSort {
+    public static void ascendBubbleSorting(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 7, 6, 8, 1, 3, 2, 9, 5 };
+        ascendBubbleSorting(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
