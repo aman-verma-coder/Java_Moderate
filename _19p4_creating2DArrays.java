dcde6e2fd3dc65f516927dde34eaff29682a8658
Aman Verma <9330nama@gmail.com>
2024-07-01 17:04:45 +0530

    _19p4_creating2DArrays.java

---
+++ _19p4_creating2DArrays.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class _19p4_creating2DArrays {
+    public static boolean search(int arr[][], int key) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if (arr[i][j] == key) {
+                    System.out.println("Element found at index (" + i + ", " + j + ")");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[][] = new int[3][3];
+        int m = arr.length;
+        int n = arr[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.print("Enter the element to search: ");
+        int key = sc.nextInt();
+        search(arr, key);
+        sc.close();
+    }
+}
